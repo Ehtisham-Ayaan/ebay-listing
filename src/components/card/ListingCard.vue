6e4ad6fed507a5d71397135431acc520653963d0
Ehtisham-Ayaan <erwattoo@gmail.com>
2022-10-30 21:47:37 +0500

    Added Dashboard

---
+++ src/components/card/ListingCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="imgBx">
+        <img src="../../assets/Logo.png">
+      </div>
+      <div class="contentBx">
+        <a href="#" @click="$emit('showListing','savermarts')">Show</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+	@import "./card.css";
+</style>
---
+++ src/components/card/ListingDetails.vue
@@ -0,0 +1,76 @@
+<template>
+	<div v-if="!add && dashboard" class="w-85 center-margin mt-2">
+		<article v-for="product in listings" :key="product.id" class="mt-1 leaderboard__profile">
+			<span class="leaderboard__name"> {{ product.name }} </span>
+			<span class="leaderboard__value">
+				<span>
+					<i 
+						class="text-dark fa-sharp fa-solid fa-eye ml-1" 
+						@click="viewProduct(product)">
+					</i>
+				</span>
+				<span>
+					<i 
+						class="text-success fa-sharp fa-solid fa-pencil ml-1" 
+						@click="editProduct(product)">
+					</i>
+				</span>
+				<span>
+					<i 
+						class="text-danger fa-sharp fa-solid fa-minus ml-1" 
+						@click="deleteProduct(product)">
+					</i>
+				</span>
+			</span>
+		</article>
+		<article class="leaderboard__profile mt-1">
+			<span class="leaderboard__name">New Item</span>
+			<span class="leaderboard__value">
+				<span>
+					<i 
+						class="text-success fa-sharp fa-solid fa-plus" 
+						@click="addProduct()">
+					</i>
+				</span>
+			</span>
+		</article>
+	</div>
+	<ListingForm v-else-if="dashboard"></ListingForm>
+	<SaverMart v-else :phone="phone"/>
+</template>
+<script>
+	import ListingForm from "./ListingForm.vue"
+  import SaverMart from '../savermart/savermart.vue'
+	export default{
+		components: {
+			ListingForm,
+			SaverMart
+		},
+		props: ["listing","listings","dashboard"],
+		data(){
+			return{
+				add: false,
+				phone: null
+			}
+		},
+		methods: {
+			addProduct(){
+				this.add = true
+			},
+			viewProduct(product){
+				this.$emit('viewProduct')
+				this.$emit('renderTemplate')
+				this.phone = product
+			},
+			editProduct(product){
+				console.log(product)
+			},
+			deleteProduct(product){
+				this.$emit("deleteListing",product)
+			},
+		} 
+	}
+</script>
+<style scoped> 
+	@import "./list.css";
+</style>
